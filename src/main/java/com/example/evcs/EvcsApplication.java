46bad449db0cfa7bed5123a0ada295736de17303
kysman2580 <165656455+kysman2580@users.noreply.github.com>
2025-04-15 21:01:14 +0900

    초기 셋팅

---
+++ src/main/java/com/example/evcs/EvcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.evcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvcsApplication.class, args);
+	}
+
+}
