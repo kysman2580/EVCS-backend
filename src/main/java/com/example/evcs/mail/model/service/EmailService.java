a676f1156198fcb4fc2491a90c8f75cf4710bd1d
kysman2580 <165656455+kysman2580@users.noreply.github.com>
2025-04-28 17:30:43 +0900

    업데이트 로직 개발 완료

--- src/main/java/com/example/evcs/mail/model/service/EmailService.java
+++ src/main/java/com/example/evcs/mail/model/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.evcs.mail.model.service;
 
 import com.example.evcs.mail.dto.EmailVerifyDTO;
+import com.example.evcs.mail.dto.PassWordEmailVerifyDTO;
 import com.example.evcs.member.model.dto.MemberDTO;
 
 public interface EmailService {
@@ -9,5 +10,9 @@ public interface EmailService {
 	void verifyCode(EmailVerifyDTO emailVerifyDTO);
 
 	void sendVerificationCode(EmailVerifyDTO emailVerifyDTO);
+
+	void sendPassWordVerificationCode(PassWordEmailVerifyDTO passWordEmailVerifyDTO);
+
+	void passwordVerifyCode(PassWordEmailVerifyDTO passWordEmailVerifyDTO);
 	
 }
